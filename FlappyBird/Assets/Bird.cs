aad0126fd8fbf99a755e69aa9aa6131e45adfa59
dinesh_gaglani@hotmail.com <dinesh_gaglani@hotmail.com>
2020-06-01 22:30:26 -0400

    Completed bird game, cleaned rest of the folder

--- FlappyBird/Assets/Bird.cs
+++ FlappyBird/Assets/Bird.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
     private Vector3 _initPosition;
-    private bool _isDragStart;
-    private void awake() {
+    private bool _isLaunched;
+    private float _idleTime;
+
+    [SerializeField] private float _force = 100;
+    private void Awake() {
         _initPosition = transform.position;
-        _isDragStart = false;
+        _isLaunched = false;
     }
 
-    private void update() {
+    private void Update() {
 
+        if(_isLaunched && GetComponent<Rigidbody2D>().velocity.magnitude < 0.1f)
+        {
+            _idleTime += Time.deltaTime;
+        }
+
+       if (Mathf.Abs(transform.position.x) > 15 
+            || Mathf.Abs(transform.position.y) > 5
+            || _idleTime > 3) {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       }
+
+        LineRenderer lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, _initPosition);
+        
     }
 
     private void OnMouseDown() {
@@ -22,18 +41,20 @@ public class Bird : MonoBehaviour
     private void OnMouseUp() {
         GetComponent<SpriteRenderer>().color = Color.white;
 
-        if (_isDragStart) {
-            Rigidbody2D bird = GetComponent<Rigidbody2D>();
-            Vector2 forceOnBird = new Vector2(_initPosition.x - transform.position.x, _initPosition.y - transform.position.y);
-            bird.AddForce(forceOnBird * 200);
-            bird.gravityScale = 1;
-        }
-        
+       
+        Rigidbody2D bird = GetComponent<Rigidbody2D>();
+        Vector2 forceOnBird = new Vector2(_initPosition.x - transform.position.x, _initPosition.y - transform.position.y);
+        Debug.Log("strength: " + _force);
+        Debug.Log("force on bird: " + forceOnBird);
+        bird.AddForce(forceOnBird * _force);
+        bird.gravityScale = 1;
+        _isLaunched = true;
+        GetComponent<LineRenderer>().enabled = false;
     }
 
     private void OnMouseDrag() {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(newPosition.x, newPosition.y, 0);
-        _isDragStart = true;
+        GetComponent<LineRenderer>().enabled = true;
     }
 }
---
+++ FlappyBird/Assets/monster/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Bird>() != null)
+        {
+            //Anything other than it collides with will break it
+            Debug.Log("Collision!");
+            Destroy(gameObject);
+        } 
+    }
+
+
+}
